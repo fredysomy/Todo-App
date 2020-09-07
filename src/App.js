a6e18bf6e3b9b139e020e712a5f21d8b69f9adbe
Fredy Somy <65971072+fredysomy@users.noreply.github.com>
2020-09-07 12:55:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component{
     render(){
         return(
             <div>
-            <h1 align="center">TODOS LIST</h1>
+            <h1 className="App" align="center">TODOS LIST</h1>
             <h3>This is your todos</h3>
             <Todos todos={this.state.todos} />
             </div>
